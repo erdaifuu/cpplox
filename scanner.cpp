fda20545c62ffb1f8629504dbdfbb4f44476789c
Carl Luo <erdaifuu@gmail.com>
2026-06-19 00:01:51 -0700

    Finished Ch. 4 and Scanner design

--- scanner.cpp
+++ scanner.cpp
@@ -1,21 +1,73 @@
 #include "scanner.h"
 #include <utility>
 
+#include "lox.h"
+
+const std::unordered_map<std::string, token_type> Scanner::keywords = {
+    {"and",    AND},
+    {"class",  CLASS},
+    {"else",   ELSE},
+    {"false",  FALSE},
+    {"for",    FOR},
+    {"fun",    FUN},
+    {"if",     IF},
+    {"nil",    NIL},
+    {"or",     OR},
+    {"print",  PRINT},
+    {"return", RETURN},
+    {"super",  SUPER},
+    {"this",   THIS},
+    {"true",   TRUE},
+    {"var",    VAR},
+    {"while",  WHILE},
+};
+
+// helpers
+
+bool Scanner::is_at_end() const
+{
+    return current >= source.length();
+}
+
+char Scanner::advance()
+{
+    return source[current++];
+}
+
+bool Scanner::match(char expected)
+{
+    if (is_at_end()) return false;
+    if (source[current] != expected) return false;
+
+    current++;
+    return true;
+}
+
+char Scanner::peek() const
+{
+    if (is_at_end()) return '\0';
+    return source[current];
+}
+
+char Scanner::peek_next() const
+{
+    if (current + 1 >= source.length()) return '\0';
+    return source[current + 1];
+}
+
+// token
+
 std::vector<Token> Scanner::scan_tokens()
 {
-    while (!isAtEnd())
+    while (!is_at_end())
     {
         // We are at the beginning of the next lexeme.
         start = current;
         scan_token();
     }
 
-    tokens.emplace_back( END_OF_FILE , "", "", line);
+    tokens.emplace_back(END_OF_FILE, "", "", line);
     return tokens;
-}
-
-bool Scanner::is_at_end() const {
-    return current >= source.length();
 }
 
 void Scanner::scan_token()
@@ -23,30 +75,122 @@ void Scanner::scan_token()
     char c = advance();
     switch (c)
     {
-        case '(': add_token(LEFT_PAREN); break;
-        case ')': add_token(RIGHT_PAREN); break;
-        case '{': add_token(LEFT_BRACE); break;
-        case '}': add_token(RIGHT_BRACE); break;
-        case ',': add_token(COMMA); break;
-        case '.': add_token(DOT); break;
-        case '-': add_token(MINUS); break;
-        case '+': add_token(PLUS); break;
-        case ';': add_token(SEMICOLON); break;
-        case '*': add_token(STAR); break;
+    case '(': add_token(LEFT_PAREN); break;
+    case ')': add_token(RIGHT_PAREN); break;
+    case '{': add_token(LEFT_BRACE); break;
+    case '}': add_token(RIGHT_BRACE); break;
+    case ',': add_token(COMMA); break;
+    case '.': add_token(DOT); break;
+    case '-': add_token(MINUS); break;
+    case '+': add_token(PLUS); break;
+    case ';': add_token(SEMICOLON); break;
+    case '*': add_token(STAR); break;
+    case '!': add_token(match('=') ? BANG_EQUAL : BANG); break;
+    case '=': add_token(match('=') ? EQUAL_EQUAL : EQUAL); break;
+    case '<': add_token(match('=') ? LESS_EQUAL : LESS); break;
+    case '>': add_token(match('=') ? GREATER_EQUAL : GREATER); break;
+    case '/':
+        if (match('/')) { while (peek() != '\n' && !is_at_end()) advance(); }
+        else { add_token(SLASH); }
+        break;
+    case ' ':
+    case '\r':
+    case '\t':
+        break;
+    case '\n': line++; break;
+
+    // String Literals
+    case '"': string(); break;
+
+    // Digits (default case to avoid manual checking all 0-9 cases)
     default:
-        Lox.error(line, "Unexpected character.");;
+        //  I used static_cast with std here, then defined is_digit later, this is just for future reference & cool
+        if (std::isdigit(static_cast<unsigned char>(c))) {
+            number();
+        } else if (is_alpha(c)) {
+            identifier();
+        } else {
+            error(line, "Unexpected character.");
+        }
     }
 }
 
-char Scanner::advance() {
-    return source[current++];
-}
-
-void Scanner::add_token(token_type type) {
+void Scanner::add_token(token_type type)
+{
     add_token(type, "");
 }
 
-void Scanner::add_token(token_type type, const std::string& literal) {
+void Scanner::add_token(token_type type, const std::string& literal)
+{
     std::string text = source.substr(start, current - start);
     tokens.emplace_back(type, text, literal, line);
+}
+
+// literal scanner helpers
+
+void Scanner::string()
+{
+    while (peek() != '"' && !is_at_end()) {
+        if (peek() == '\n') line++;
+        advance();
+    }
+
+    if (is_at_end()) {
+        error(line, "Unterminated string.");
+        return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    std::string value = source.substr(start + 1, current - start - 2);
+    add_token(STRING, value);
+}
+
+void Scanner::number()
+{
+    while (is_digit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && is_digit(peek_next())) {
+        // Consume the "."
+        advance();
+
+        while (is_digit(peek())) advance();
+    }
+
+    // TODO: add_token can actually take other types (in Java we used Object, in C++ ?)
+    // add_token(NUMBER, std::stod(source.substr(start, current - start)));
+    add_token(NUMBER, source.substr(start, current - start));
+}
+
+void Scanner::identifier()
+{
+    while (is_alphanumeric(peek())) advance();
+
+    std::string text = source.substr(start, current - start);
+    token_type type = IDENTIFIER;
+    auto it = keywords.find(text);
+    if (it != keywords.end()) type = it->second;
+    add_token(type);
+}
+
+// static character classification
+
+bool Scanner::is_digit(const char c)
+{
+    return c >= '0' && c <= '9';
+}
+
+bool Scanner::is_alpha(const char c)
+{
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+}
+
+bool Scanner::is_alphanumeric(const char c)
+{
+    return is_alpha(c) || is_digit(c);
 }
--- scanner.h
+++ scanner.h
@@ -2,6 +2,7 @@
 #define CPPLOX_SCANNER_H
 
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 #include "token_type.h"
@@ -15,14 +16,32 @@ public:
     std::vector<Token> scan_tokens();
 
 private:
+    // helpers
     bool is_at_end() const;
-    void scan_token();
     char advance();
+    bool match(char expected);
+    char peek() const;
+    char peek_next() const;
+
+    // token
+    void scan_token();
     void add_token(token_type type);
     void add_token(token_type type, const std::string& literal);
 
+    // literal scanner helpers
+    void string();
+    void number();
+    void identifier();
+
+    // static char classification
+    static bool is_digit(char c);
+    static bool is_alpha(char c);
+    static bool is_alphanumeric(char c);
+
+    // variables
     const std::string source;
     std::vector<Token> tokens;
+    static const std::unordered_map<std::string, token_type> keywords;
 
     int start{};
     // TODO: we use current to grab token substrings, but in C++ we use start_index + length for that. Swap out with length eventually.
