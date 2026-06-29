c8536318ce0af3f18c57dec5021f292a713fd8d3
Erdaifu Luo <erdaifuu@gmail.com>
2026-06-28 23:59:17 -0700

    Interpretor class

---
+++ interpreter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Carl Luo on 28/06/2026.
+//
+
+#include "interpreter.h"
---
+++ interpreter.h
@@ -0,0 +1,15 @@
+//
+// Created by Carl Luo on 28/06/2026.
+//
+
+#ifndef CPPLOX_INTERPRETER_H
+#define CPPLOX_INTERPRETER_H
+
+#include "expr.h"
+
+class interpreter : public visitor
+{
+};
+
+
+#endif //CPPLOX_INTERPRETER_H
